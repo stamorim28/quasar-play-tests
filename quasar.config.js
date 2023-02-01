e44f2129c5a4a4146c6da9eaf79efbea2e91ee5f
Stenio <jstenio.rocha@gmail.com>
2023-02-01 20:26:26 -0300

    feat: add LocalStorage plugin

--- quasar.config.js
+++ quasar.config.js
@@ -8,6 +8,7 @@
 // Configuration for your app
 // https://v2.quasar.dev/quasar-cli-vite/quasar-config-js
 
+const { LocalStorage } = require("quasar");
 const { configure } = require("quasar/wrappers");
 
 module.exports = configure(function (/* ctx */) {
@@ -99,7 +100,7 @@ module.exports = configure(function (/* ctx */) {
       // directives: [],
 
       // Quasar plugins
-      plugins: [],
+      plugins: ["LocalStorage"],
     },
 
     // animations: 'all', // --- includes all animations
