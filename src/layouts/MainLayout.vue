1cf1e6fd472192ccf21910f0cb5f127b9e3e49de
Stenio <jstenio.rocha@gmail.com>
2023-02-02 19:31:15 -0300

    style: change some styles

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -11,7 +11,7 @@
           @click="toggleLeftDrawer"
         />
 
-        <q-toolbar-title> Quasar App </q-toolbar-title>
+        <q-toolbar-title> Countries App </q-toolbar-title>
 
         <DarkModeToggle />
 
